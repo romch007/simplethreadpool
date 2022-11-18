f5e9d5c85bb36f5dc918ff287cfaa8f1999a479e
Romain Chardiny <romain.chardiny@gmail.com>
2022-11-18 13:26:57 +0100

    support wasm :)

--- xmake.lua
+++ xmake.lua
@@ -10,7 +10,18 @@ set_rundir(".")
 set_targetdir("./build/$(plat)_$(arch)_$(mode)")
 
 if is_plat("wasm") then
+    add_cxxflags("-sPTHREAD_POOL_SIZE=20")
+    add_cxxflags("-sALLOW_MEMORY_GROWTH")
+    add_cxxflags("-pthread")
+    add_ldflags("-sPTHREAD_POOL_SIZE=20")
+    add_ldflags("-sALLOW_MEMORY_GROWTH")
+    add_ldflags("-pthread")
     add_defines("__WASM__")
+    if is_mode("debug") then
+        add_cxxflags("-sASSERTIONS=2")
+        add_cxxflags("-sSAFE_HEAP=1")
+        add_cxxflags("-sDEMANGLE_SUPPORT=1")
+    end
 end
 
 includes("tests/xmake.lua")
