eebb65a70446f391cc4d1202e78cf176f7591a63
Romain Chardiny <romain.chardiny@gmail.com>
2022-11-18 14:10:02 +0100

    support bsd

--- include/simplethreadpool/config.hpp
+++ include/simplethreadpool/config.hpp
@@ -4,32 +4,35 @@
 #define SIMPLETHREADPOOL_CONFIG_HPP
 
 #if defined(_WIN32)
-#define SIMPLETHREADPOOL_EXPORT __declspec(dllexport)
-#define SIMPLETHREADPOOL_IMPORT __declspec(dllimport)
+  #define SIMPLETHREADPOOL_EXPORT __declspec(dllexport)
+  #define SIMPLETHREADPOOL_IMPORT __declspec(dllimport)
 #elif defined(__ANDROID__)
-#define SIMPLETHREADPOOL_EXPORT __attribute__((visibility("default")))
-#define SIMPLETHREADPOOL_IMPORT __attribute__((visibility("default")))
+  #define SIMPLETHREADPOOL_EXPORT __attribute__((visibility("default")))
+  #define SIMPLETHREADPOOL_IMPORT __attribute__((visibility("default")))
 #elif defined(__linux__)
-#define SIMPLETHREADPOOL_EXPORT __attribute__((visibility("default")))
-#define SIMPLETHREADPOOL_IMPORT __attribute__((visibility("default")))
-#elif defined(__FreeBSD__)
-#define SIMPLETHREADPOOL_EXPORT __attribute__((visibility("default")))
-#define SIMPLETHREADPOOL_IMPORT __attribute__((visibility("default")))
+  #define SIMPLETHREADPOOL_EXPORT __attribute__((visibility("default")))
+  #define SIMPLETHREADPOOL_IMPORT __attribute__((visibility("default")))
+#elif defined(__unix__) || !defined(__APPLE__) && defined(__MACH__)
+  #include <sys/param.h>
+  #if defined(BSD)
+    #define SIMPLETHREADPOOL_EXPORT __attribute__((visibility("default")))
+    #define SIMPLETHREADPOOL_IMPORT __attribute__((visibility("default")))
+  #endif
 #elif defined(__WASM__)
-#define SIMPLETHREADPOOL_EXPORT
-#define SIMPLETHREADPOOL_IMPORT
+  #define SIMPLETHREADPOOL_EXPORT
+  #define SIMPLETHREADPOOL_IMPORT
 #else
-#error "Platform not supported"
+  #error "Platform not supported"
 #endif
 
 #if !defined(SIMPLETHREADPOOL_STATIC)
-#ifdef SIMPLETHREADPOOL_BUILD
-#define SIMPLETHREADPOOL_API SIMPLETHREADPOOL_EXPORT
+  #ifdef SIMPLETHREADPOOL_BUILD
+    #define SIMPLETHREADPOOL_API SIMPLETHREADPOOL_EXPORT
+  #else
+    #define SIMPLETHREADPOOL_API SIMPLETHREADPOOL_IMPORT
+  #endif
 #else
-#define SIMPLETHREADPOOL_API SIMPLETHREADPOOL_IMPORT
-#endif
-#else
-#define SIMPLETHREADPOOL_API
+  #define SIMPLETHREADPOOL_API
 #endif
 
 #endif
